be6ab3f7432c48b9384e15375aa0161b642975a6
Goober2112 <mlemixservice@gmail.com>
2024-08-02 20:59:39 -0400

    Update Delta.lua

--- scripts/ui/Delta.lua
+++ scripts/ui/Delta.lua
@@ -76,7 +76,7 @@ end
 
 if not isfile("new_logo.png") then
     local new_logo = game:HttpGet("https://raw.githubusercontent.com/delta-hydro/secret-host-haha/main/lxnnypfp7.png")
-    writefile("new_logo.png", new_logo.Body)
+    writefile("new_logo.png", new_logo)
 end
 
 

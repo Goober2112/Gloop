40a31ffa602af6aca07a2cf1150c341f9344875b
Goober2112 <mlemixservice@gmail.com>
2024-10-22 00:57:51 +0000

    Update Delta.lua

--- scripts/ui/Delta.lua
+++ scripts/ui/Delta.lua
@@ -5831,18 +5831,12 @@ coroutine.wrap(function()
 end)()
 
 function checkkey()
-	--local keyless = game:HttpGet("https://raw.githubusercontent.com/lxnnydev/DeltaAndroid/main/iskeyless")
-	--if string.find(keyless, "true") then
-	--GrantAccess()
-        --return true
-	--end
-
---if Verify() then
+if Verify() then
       GrantAccess()
       return true
---end
+end
 
---return false
+return false
 end
 
 

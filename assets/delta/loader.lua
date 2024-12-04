b8819436f127b6fd505b78463175cf8b67d35d98
Goober2112 <mlemixservice@gmail.com>
2024-12-04 03:19:28 +0000

    Update loader.lua

--- assets/delta/loader.lua
+++ assets/delta/loader.lua
@@ -1,22 +1,21 @@
-
 local deltaAssets = {
+    ["13569242972"] = "cb0457617b0bfbda86688b261a2d312b",
+    ["13365156882"] = "1b87ff08021f3e9fac662f14a022eb59",
+    ["13462269327"] = "da23712d4d8098995abfc585488fb95a",
+    ["13365407660"] = "28478d0b937d617c0c36b9242f75a71f",
+    ["13387875723"] = "b62a7595743d2cf9a84fe885fe2308b4",
+    ["13387419794"] = "fbaae0cbc9e27afddd255cc1e4283934",
+    ["13462268450"] = "160dfe9eebe85925be0cba453d6ac21a",
+    ["13363093418"] = "b93c4f60d3f7dd9aa2eaf689a742c8c5",
+    ["13363121645"] = "6fc46a06072a1d3e8c99945da150c654",
     ["11702779517"] = "4897023ca03071d11c3adb84c557429c",
-    ["13363093418"] = "6a4acaddea30c139640bbf13b7da64ed",
-    ["13363121645"] = "a32f37bfebe4bb1854d72e85455a3050",
-    ["13365156882"] = "3d821645411c057be6ead687d6ae51c5",
-    ["13365407660"] = "76e95a29d8e77ccce2fd8a53afe152c3",
-    ["13387419794"] = "c2297530c18fef96d1dc86773ed06c7d",
-    ["13387627918"] = "7ad72c091d4b6312bcbeb3cde4073c1c",
-    ["13387657138"] = "7a74c177ddc9caca469bc51c6f9785f8",
-    ["13387875723"] = "e524e73791897c86f635a5cc0d464cd2",
-    ["13441628967"] = "de0db057baabad3b87a2469b851f07c6",
-    ["13441695981"] = "e272fbb6fe0084b671354da4b748170a",
-    ["13462268450"] = "cf0226342eaa00afc29db9e43f60747a",
-    ["13462269327"] = "a20730b296d2ae97b9da282297c3c939",
-    ["13462270380"] = "5602c7de756a0c644949924c80e9119e",
-    ["13462271348"] = "e98af78d7c0410ccf1602662142c4f35",
-    ["13569242972"] = "f92102d598550f8bfef2c62d2172ea9f",
-    ["6014261993"] = "c616ae90426588ef3cd13240b3bf6060"
+    ["13441628967"] = "8b736435badb78835695b827b533ce73",
+    ["13462271348"] = "4ad52c7bd0e16462865bdf5bb68205b5",
+    ["6014261993"] = "fdfaf5d8fa0056b0bd1a0c23a5e5f8ff",
+    ["13441695981"] = "6dd24f0dfdf15a84373272177d88828f",
+    ["13462270380"] = "bd8fc053bb4bae6f6047beb73965338c",
+    ["13387657138"] = "ed6e8bee546663c48bb07e773523ad5c",
+    ["13387627918"] = "763397d350f8d7f7b8e2faaf8cbb7005"
 }
 
 print("isfolder" .. tostring(isfolder))
@@ -36,17 +35,12 @@ for i, v in next, deltaAssets do
 end
 
 function loadAssets()
-    print("checking folder")
     if not isfolder("DeltaAssets") then
-        print("making folder")
         makefolder("DeltaAssets")
     end
 
-    print("loopin")
     for assetId, md5 in next, deltaAssets do
-        print("putin")
         local assetPath = "./DeltaAssets/" .. assetId
-        print("checking asset: " .. assetPath)
         if not isfile(assetPath) or md5 ~= crypt.hash(readfile(assetPath), "md5") then
             if isfile(assetPath) then
                 local l = readfile(assetPath)
@@ -54,8 +48,6 @@ function loadAssets()
                 
                 print("expected md5: " .. md5 .. " got: " .. m .. " size: " .. #l)
             end
-            
-            print("downloading asset: " .. assetPath)
 
             local success, err = pcall(function()
                 local content = game:HttpGetAsync(
@@ -100,6 +92,3 @@ return {
     getAsset = getAsset,
     getProgress = getProgress
 }
-
-
-

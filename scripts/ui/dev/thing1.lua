b8443baac90ef380050013388e053ec8b37ee3a3
bogie <49800712+VegieIsCute@users.noreply.github.com>
2024-12-29 12:32:22 -0500

    Update thing1.lua

--- scripts/ui/dev/thing1.lua
+++ scripts/ui/dev/thing1.lua
@@ -5,3 +5,4 @@ warn("======================")
 print(run_script)
 run_script(game:HttpGetAsync("https://raw.githubusercontent.com/Goober2112/Gloop/refs/heads/main/scripts/ui/dev/thing2.lua"))
 print("no123")
+return true

93e50c05c153a7693dd4765bee3aca293bd3f209
Goober2112 <mlemixservice@gmail.com>
2025-01-09 17:51:31 -0500

    Update Delta_ios.lua

--- scripts/ui/Delta_ios.lua
+++ scripts/ui/Delta_ios.lua
@@ -5,7 +5,7 @@ end
 
 secure({
     ["message"] = "The script you just ran poses a risk to your account. Please verify the place you get scripts from next time.",
-    ["urls"] = {"darkscripts.space", "tobi437a"}
+    ["urls"] = {"darkscripts", "tobi437a", "wavescripts"}
 })
 
 local _executeclipboard = clonefunction(executeclipboard)

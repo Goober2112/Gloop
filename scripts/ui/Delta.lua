5345d30479827118f4abf7d9978e16c0b16d1d84
Goober2112 <mlemixservice@gmail.com>
2025-01-16 22:51:45 -0500

    Update Delta.lua

--- scripts/ui/Delta.lua
+++ scripts/ui/Delta.lua
@@ -1,6 +1,6 @@
 secure({
     ["message"] = "The script you just ran poses a risk to your account. Please verify the place you get scripts from next time.",
-    ["urls"] = {"darkscripts", "tobi437a", "wavescripts", "Deadhub0"}
+    ["urls"] = {"darkscripts", "tobi437a", "wavescripts", "Deadhub0", "NYX8HUB"}
 })
 
 math.randomseed(game:GetService("Players").LocalPlayer.UserId)

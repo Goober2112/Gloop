a0b04e612a4ff3a54ba4636165735d142b7e7b3d
Goober2112 <mlemixservice@gmail.com>
2025-01-18 13:20:01 -0500

    Update delta-anti-scam.lua

--- assets/delta/delta-anti-scam.lua
+++ assets/delta/delta-anti-scam.lua
@@ -2,6 +2,17 @@ local LocalPlayer = cloneref(game:GetService("Players").LocalPlayer);
 local LocalKick = clonefunction(LocalPlayer.Kick);
 local GameId = game.GameId;
 
+local function become_silly(str)
+	local function guah(s)
+  		return s:lower() == s and ('a'):byte() or ('A'):byte()
+	end
+	
+  	return (str:gsub('%a', function(s)
+    		local base = guah(s)
+    		return string.char(((s:byte() - base + (-13)) % 26) + base)
+  	end))
+end
+
 if GameId == 3317771874 then
 	local remote_hook_nc = nil;
 	remote_hook_nc = hookmetamethod(game, "__namecall", function(...) 
@@ -9,8 +20,8 @@ if GameId == 3317771874 then
 		if checkcaller() and namecallmethod == "InvokeServer" then
 			local args = {...}
 			if args[1].Name == "Mailbox: Send" then
-				LocalKick(LocalPlayer, "This script just tried to take your items! Do NOT run unknown scripts!!")
-				error("DELTA STOPPED YOU FROM LOSING ITEMS")
+				LocalKick(LocalPlayer, become_silly("Guvf fpevcg whfg gevrq gb FGRNY lbhe CRGF! Qb ABG eha haxabja fpevcgf!!"))
+				error(become_silly("QRYGN WHFG CERIRAGRQ LBH SEBZ YBFVAT LBHE VGRZF"))
 			end
 		end
 		return remote_hook_nc(...)
@@ -24,8 +35,8 @@ elseif GameId == 3317679266 then
 		if checkcaller() then
 			local args = {...}
 			if type(args[1]) == "string" and args[1] == "OfflinePlayerLookup" then
-				LocalKick(LocalPlayer, "This script just tried to STEAL your ROBUX! Please do NOT run unknown scripts!!!")
-				error("DELTA STOPPED YOU FROM LOSING ROBUX")
+				LocalKick(LocalPlayer, become_silly("Guvf fpevcg whfg gevrq gb FGRNY lbhe EBOHK! Cyrnfr qb ABG eha haxabja fpevcgf!!"))
+				error(become_silly("QRYGN WHFG CERIRAGRQ LBH SEBZ YBFVAT EBOHK"))
 			end
 		end
 		return og_function(...)

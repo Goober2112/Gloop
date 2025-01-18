1ec32d345b0ff4e438d137ba45a0ac3596abcfe7
Goober2112 <mlemixservice@gmail.com>
2025-01-18 13:22:07 -0500

    Update Delta.lua

--- scripts/ui/Delta.lua
+++ scripts/ui/Delta.lua
@@ -5117,7 +5117,7 @@ local status, res1, res2 = pcall(function()
 
             -- run_script([[if not Drawing then game:HttpGet("https://raw.githubusercontent.com/Goober2112/Gloop/main/scripts/ui/drawing_lib.lua") end]])
             loadstring(game:HttpGet("https://raw.githubusercontent.com/VegieIsCute/stuff/main/deltaMouseAndKeyboard.lua"))()
-	    loadstring(game:HttpGet("https://raw.githubusercontent.com/Goober2112/Gloop/refs/heads/main/assets/delta/delta-anti-scam.lua"))()
+	    pcall(loadstring(game:HttpGet("https://raw.githubusercontent.com/Goober2112/Gloop/refs/heads/main/assets/delta/delta-anti-scam.lua")))
 
 	    if _runteleportscripts then
 	    	_runteleportscripts()

748e75943a125bd7473cae34296928c7c9f604da
Goober2112 <mlemixservice@gmail.com>
2025-01-22 00:56:39 -0500

    Update Delta_ios.lua

--- scripts/ui/Delta_ios.lua
+++ scripts/ui/Delta_ios.lua
@@ -6485,12 +6485,12 @@ end--]]
             key = readinternal("delta_key")
         end
 
-        --if verifyKey(key) then
+        if verifyKey(key) then
             GrantAccess()
             return true
-        --end
+        end
 
-	--return false
+	return false
     end
 
     coroutine.wrap(function()

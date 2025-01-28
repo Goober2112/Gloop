6389649fe466993abc9a21d38e583bff53ce8c42
Goober2112 <mlemixservice@gmail.com>
2025-01-27 21:37:46 -0500

    Update Delta.lua

--- scripts/ui/dev/Delta.lua
+++ scripts/ui/dev/Delta.lua
@@ -77,11 +77,11 @@ local status, res1, res2 = pcall(function()
                 return false, msg;
             end
 
-	    local exn, exv = identifyexecutor();
+	    --[[local exn, exv = identifyexecutor();
 	    if response.StatusCode == nil and exv ~= "1.5.656.677" then
                 setclipboard("https://deltaexploits.gg/android_dl")
 		disconnect("FAILED TO CACHE LINK ERRORS FIXED IN THE LATEST DELTA AT https://deltaexploits.gg/android_dl (COPIED TO CLIPBOARD)");
-	    end
+	    end--]]
 
             local msg = "Failed to cache link. (" .. tostring(response.StatusCode) .. ")";
             onMessage(msg);

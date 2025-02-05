63d4700eed72c0ed5074b6e50855b3aa1385e01e
Goober2112 <mlemixservice@gmail.com>
2025-02-05 00:54:01 -0500

    Update Delta.lua

--- scripts/ui/Delta.lua
+++ scripts/ui/Delta.lua
@@ -788,12 +788,12 @@ local status, res1, res2 = pcall(function()
         end)()
     end
 
-    if execversion ~= "1.0.658.460" and (game.PlaceId == 16732694052) then
+    --[[if execversion ~= "1.0.658.460" and (game.PlaceId == 16732694052) then
         local message = fStringFormat(searchTranslationVector(translationVector2, sLocaleId), execversion);
         showModal(title, message, url);
         error(message);
         return
-    end
+    end--]]
 
     --[[
     for _, outdatedVersion in next, versionsToUpdate do

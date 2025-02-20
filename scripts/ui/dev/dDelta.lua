bc0f6b3f7ff2e8cd8cd15e4973b70b39e1044abc
Goober2112 <mlemixservice@gmail.com>
2025-02-20 16:30:13 -0500

    Update dDelta.lua

--- scripts/ui/dev/dDelta.lua
+++ scripts/ui/dev/dDelta.lua
@@ -1,9 +1,3 @@
-if not game:IsLoaded() then
-	repeat
-		task.wait()
-	until game:IsLoaded();
-end
-		
 secure({
     ["preload"] = true, --loadstring(secretstring("dJZ2fICV2It1m5iAvHWDnXrnldjb3szc3d3XzFnpz1lUXdzo6ene3aX5oZS5kvzGvcKbwJ7mtnTGyqo="))(),
     ["message"] = secretstring("VomTftJ4mZF9iZC9g3qJ13SjgaafrqDlipLOf6WplKhkp7Gl2JWbuo25p9W2krudt/2X54/Q3+R9KwzZzLK7sqUFxNi6tLi/ELHP28fSIsW/zMfNxd3v2iLG6h3Sytniz9PmE9vcFe3h2+DY8e8="),
@@ -311,6 +305,12 @@ local status, res1, res2 = pcall(function()
         end
     end
     -------------------------------------------------------------------------------
+    if not game:IsLoaded() then
+	repeat
+		task.wait()
+	until game:IsLoaded();
+    end
+		
     local loader = loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/Goober2112/Gloop/refs/heads/main/scripts/ui/dev/dev_loader.lua"))()
     local loadAssets, getAsset, getProgress = loader.loadAssets, loader.getAsset, loader.getProgress
 

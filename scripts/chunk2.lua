045e695e0a05ecbd5d8dcd1a58ed91bdf9518596
bogie <49800712+VegieIsCute@users.noreply.github.com>
2025-03-23 13:07:29 -0400

    Update chunk2.lua

--- scripts/chunk2.lua
+++ scripts/chunk2.lua
@@ -1,8 +1,86 @@
-local DENTA, BENTA, GENTA = identifyexecutor();
+-- Gui to Lua
+-- Version: 3.2
 
-function a() end; function b() end;
+-- Instances:
 
-local old; old = hookfunction(a, b);
+local ScreenGui = Instance.new("ScreenGui")
+local Frame = Instance.new("Frame")
+local TextBox = Instance.new("TextBox")
+local TextButton = Instance.new("TextButton")
+local TextButton_2 = Instance.new("TextButton")
+local TextButton_3 = Instance.new("TextButton")
+local TextButton_4 = Instance.new("TextButton")
 
-clonefunction(a)
-clonefunction(b)
+--Properties:
+
+ScreenGui.Parent = gethui()
+ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+
+Frame.Parent = ScreenGui
+Frame.BackgroundColor3 = Color3.fromRGB(124, 124, 124)
+Frame.BorderColor3 = Color3.fromRGB(0, 0, 0)
+Frame.BorderSizePixel = 0
+Frame.Position = UDim2.new(0.256390393, 0, 0.230031952, 0)
+Frame.Size = UDim2.new(0, 353, 0, 230)
+
+TextBox.Parent = Frame
+TextBox.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextBox.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.BorderSizePixel = 0
+TextBox.Size = UDim2.new(0, 353, 0, 186)
+TextBox.ClearTextOnFocus = false
+TextBox.Font = Enum.Font.SourceSans
+TextBox.Text = "print(\"best ui\")"
+TextBox.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextBox.TextSize = 14.000
+TextBox.TextWrapped = true
+TextBox.TextXAlignment = Enum.TextXAlignment.Left
+TextBox.TextYAlignment = Enum.TextYAlignment.Top
+
+TextButton.Parent = Frame
+TextButton.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.BorderSizePixel = 0
+TextButton.Position = UDim2.new(0, 0, 0.886956513, 0)
+TextButton.Size = UDim2.new(0, 87, 0, 26)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Execute"
+TextButton.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton.TextSize = 14.000
+
+TextButton.MouseButton1Click:Connect(function()
+    run_script(TextBox.Text)
+end)
+
+TextButton_2.Parent = Frame
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_2.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.BorderSizePixel = 0
+TextButton_2.Position = UDim2.new(0.303116143, 0, 0.886956513, 0)
+TextButton_2.Size = UDim2.new(0, 103, 0, 26)
+TextButton_2.Font = Enum.Font.SourceSans
+TextButton_2.Text = "Execute Clipboard"
+TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextSize = 14.000
+
+TextButton_3.Parent = Frame
+TextButton_3.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_3.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_3.BorderSizePixel = 0
+TextButton_3.Position = UDim2.new(0.648725212, 0, 0.886956513, 0)
+TextButton_3.Size = UDim2.new(0, 53, 0, 26)
+TextButton_3.Font = Enum.Font.SourceSans
+TextButton_3.Text = "Clear"
+TextButton_3.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_3.TextSize = 14.000
+
+TextButton_4.Parent = Frame
+TextButton_4.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextButton_4.BorderColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_4.BorderSizePixel = 0
+TextButton_4.Position = UDim2.new(0.849858344, 0, 0.886956513, 0)
+TextButton_4.Size = UDim2.new(0, 53, 0, 26)
+TextButton_4.Font = Enum.Font.SourceSans
+TextButton_4.Text = "IY"
+TextButton_4.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_4.TextSize = 14.000
